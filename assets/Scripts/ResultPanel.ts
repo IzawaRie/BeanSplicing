c01a08d78d4e01e173c6a4777f873409c35bacce
huangkun <1944366169@qq,com>
2026-04-13 00:03:14 +0800

    修改字体文件

--- assets/Scripts/ResultPanel.ts
+++ assets/Scripts/ResultPanel.ts
@@ -212,7 +212,7 @@ export class ResultPanel extends Component {
         }
         if (this.time_number) {
             // 格式化为 "XXs"
-            this.time_number.string = `${timeUsed} s`;
+            this.time_number.string = `${timeUsed}s`;
         }
 
         console.log(`结果统计: 正确=${rightCount}, 错误=${wrongCount}, 正确率=${percent}%, 高亮率=${highlightPercent}%, 熨烫率=${ironPercent}%, 用时=${timeUsed}秒`);
