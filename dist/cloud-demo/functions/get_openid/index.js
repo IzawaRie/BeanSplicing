b77974b1a336a36c9f188b622c4fc7a3c821918f
huangkun <1944366169@qq.com>
2026-04-14 18:13:00 +0800

    新增云函数部署文件夹

---
+++ dist/cloud-demo/functions/get_openid/index.js
@@ -0,0 +1,13 @@
+﻿exports.main = async function (event = {}) {
+  const openid = event?.userInfo?.openId || null;
+  const appid = event?.userInfo?.appId || null;
+
+  return {
+    ok: Boolean(openid),
+    openid,
+    appid,
+    message: openid
+      ? 'success'
+      : 'OPENID not found. Call this function from a WeChat Mini Game with wx.cloud.callFunction.'
+  };
+};
